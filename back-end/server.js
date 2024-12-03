d9751b543214cf11b227635473fe6f196729ec2e
Allenpt221 <Allenpt221@gmail.com>
2024-12-03 21:19:27 +0800

    creating a update for the a database

--- back-end/server.js
+++ back-end/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
-import Product from './models/roduct.model.js';
+import mongoose from 'mongoose';
+
+import Product from './models/product.model.js';
 dotenv.config();
 
 const app = express();
@@ -36,6 +38,24 @@ app.post("/api/products", async (req, res) => {
  }
 });
 
+app.put("/api/products/:id", async (req, res) => {
+    const { id } = req.params;
+    const product = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({success: false, message: "Invalid product ID"});
+    }
+
+    try {
+       const updatedProduct =  await Product.findByIdAndUpdate(id, product, {new: true});
+       res.status(200).json({success: true, data: updatedProduct});
+    } catch(error){
+        console.error(error);
+        res.status(500).json({success: false, data: "Server is Error"});
+    }
+});
+
+
 app.delete("/api/products/:id", async (req, res) =>{
     const { id } = req.params;
 
@@ -47,7 +67,7 @@ app.delete("/api/products/:id", async (req, res) =>{
     }
 })
 
-app.listen(5000, () =>{
+app.listen(PORT, () =>{
     connectDB();
-    console.log("Server is started at http://localhost:5000");
+    console.log("Server is started at http://localhost:" + PORT);
 });
